478489369bbd611db3e335b3966545ff32eb26d7
mumtaz1000 <shereenfatima1000@gmail.com>
2022-10-17 23:32:18 +0200

    Path variable annotation

--- src/main/java/com/springboot/springbootdemo/Student/StudentController.java
+++ src/main/java/com/springboot/springbootdemo/Student/StudentController.java
@@ -2,7 +2,11 @@
 package com.springboot.springbootdemo.Student;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -11,4 +15,28 @@ public class StudentController {
     public Student getStudent() {
         return new Student("Ramesh","Fadatare");
     }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Sara","Malik"));
+        students.add(new Student("Muhammad","Ali"));
+        students.add(new Student("Puja","Sharma"));
+        return students;
+    }
+
+    //Creating Rest API with path variable
+    // for example http://localhost:8080/student/id
+    //@PathVariable to bind the request URL template path variable
+    //to the method variable
+
+    @GetMapping("{firstName}/{lastName}")
+    public Student studentPathVariable(
+            @PathVariable("firstName")
+            String firstName,
+            @PathVariable("lastName")
+            String lastName) {
+        return new Student(firstName, lastName);
+    }
+
 }
